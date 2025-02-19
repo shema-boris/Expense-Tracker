7bb862618b920c3e3cc13f17ead2a660e0075da7
Shema Boris Benimana <sheboris@gmail.com>
2025-02-19 17:03:54 -0600

    added index.js & .env

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require( "dotenv");
+
+dotenv.config();
+const app=express();
+
+//MIDDLEWARE
+app.use(cors());
+
+//DB CONNECTION
+mongoose.connect(process.env.DB_CONNECTION).then(() => {
+    console.log('DB connection is successful')
+}).catch((err) => {
+    console.log(err)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on post $ {process.env.PORT}`);
+})
